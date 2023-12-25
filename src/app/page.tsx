c1e878aabe170784e85e397dd7b12f00da9bf83f
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2023-12-25 16:05:15 -0300

    add vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-'use client'
 import Head from 'next/head'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
-import ReactGA from "react-ga"
 import "@/src/app/scss/index.scss"
-import { useEffect } from 'react'
 export default function Home() {
- console.log(process.env.googleAnalyticsID)
-  useEffect(() => {
-    ReactGA.initialize(process.env.googleAnalyticsID || '');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+ 
   return (
     <div>
       <Head>
