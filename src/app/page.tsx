973b3aa06db41969bbe1bc5137ebb8996541ef84
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2023-12-25 15:58:26 -0300

    fix google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+'use client'
 import Head from 'next/head'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
-import ReactGA from "react-ga4"
+import ReactGA from "react-ga"
 import "@/src/app/scss/index.scss"
+import { useEffect } from 'react'
 export default function Home() {
-
-  ReactGA.initialize("G-5GP7K4FNZ1")
-  ReactGA.send({ hitType: 'pageview', page: "/" });
   
+  useEffect(() => {
+    if(process.env.googleAnalyticsID && process.env.NODE_ENV === "production") { // Checks for GA ID and only turns on GA in production
+      ReactGA.initialize(process.env.googleAnalyticsID);
+      ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
+    }
+  });
   return (
     <div>
       <Head>
