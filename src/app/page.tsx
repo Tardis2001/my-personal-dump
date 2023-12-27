50fea4c86e975c4a2dd8f49ca8ef22689a58e51d
Matheus Santos Evangelista <matheus_santos_evangelista@hotmail.com>
2023-12-27 18:02:22 -0300

    google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
 import "@/src/app/scss/index.scss"
 import { GoogleAnalytics } from "nextjs-google-analytics";
-
+import Script from 'next/script'
 export default function Home() {
  
   return (
@@ -11,6 +11,16 @@ export default function Home() {
       <Head>
         
       <GoogleAnalytics trackPageViews />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
         <title>My personal dump</title>
         <meta name='description' content='Criado para exclusivamente meu uso pessoal' />
         <link rel='icon' href='@/src/app/favicon.ico' />
